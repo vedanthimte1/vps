fef2c916bc4958f0e1d5a5220cab2ff6e9add8ec
vedanthimte1 <146501797+vedanthimte1@users.noreply.github.com>
2023-09-30 10:26:10 +0000

    final Scripts

--- connect.sh
+++ connect.sh
@@ -1,11 +1,16 @@
+# VPS with gui and interconnected network 
+# Script Autor : VEDANT HIMTE 
+
+networkId = " "
+
 curl -s https://install.zerotier.com | sudo bash
 
 sudo echo 'alias start="sudo service zerotier-one restart"' >> /root/.bashrc 
-sudo echo 'alias connect="sudo zerotier-cli join 363c67c55ac194ca"' >> /root/.bashrc 
+sudo echo 'alias connect="sudo zerotier-cli join $networkId"' >> /root/.bashrc 
 
 sudo echo 'alias start="sudo service zerotier-one restart"' >> /home/codespace/.bashrc
-sudo echo 'alias connect="sudo zerotier-cli join 363c67c55ac194ca"' >> /home/codespace/.bashrc
+sudo echo 'alias connect="sudo zerotier-cli join $networkId"' >> /home/codespace/.bashrc
  
 sudo service zerotier-one restart
-sudo zerotier-cli join 363c67c55ac194ca
+sudo zerotier-cli join $networkId
 
